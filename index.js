d8bdad9f55be910b6ca0801e32582c2d66e8cd04
saket <sktmakhija@gmail.com>
2021-07-30 19:51:10 +0530

    made file structure

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use('/', require('./routes'));
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`error is ${err}`);
+    }
+    console.log(`server is running on port ${port}`);
+});
